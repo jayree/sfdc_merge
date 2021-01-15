840558e3fa9a048b84afc5c7277851906489464d
jayree <jayree.git@icloud.com>
2021-01-15 22:19:49 +0100

    fix: fix installation

--- src/utils/package-json.ts
+++ src/utils/package-json.ts
@@ -1,5 +1,4 @@
 import {join} from 'path'
-import {sync as pkgDirsync} from 'pkg-dir'
 
 class PackageJson {
   public path: string
@@ -9,10 +8,7 @@ class PackageJson {
   public version: string
 
   constructor() {
-    this.path =
-      pkgDirsync(join(__dirname, '..', '..')) ||
-      process.env.INIT_CWD ||
-      process.cwd()
+    this.path = process.cwd()
     this.name = require(join(this.path, 'package.json')).name
     this.version = require(join(this.path, 'package.json')).version
   }
